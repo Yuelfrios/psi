515cc7a7bd5854837b39a65131f3efee506668b1
yuel_frios <nlara@andreani.com>
2024-05-19 22:54:35 -0300

    se agrego un control de envios repetidos(tercera columna)

--- psi.py
+++ psi.py
@@ -8,16 +8,37 @@
 from tkinter import Tk,Entry,Listbox,END
 from pandas import DataFrame,read_excel
 from os import path
+
+
+#contador de pallet
+pallet = [0]
+
+
+
+abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+contador_de_letras = 0
+
 #ventana
 ventana = Tk()
 ventana.title("Psi by nlara")
 ventana.geometry("430x900+0+10")
 
-contador_de_letras = 0
-abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-#contador de pallet
-pallet = [0]
+
+
+
+#Barra scroll
+def on_mousewheel(event):
+    # Ajuste para Windows y otros sistemas operativos
+    if event.delta:
+        delta = event.delta // 120
+    else:
+        delta = -1 if event.num == 5 else 1  # Para sistemas que usan botones 4 y 5 para el desplazamiento
+
+    tabla.yview_scroll(-1 * delta, "units")
+    tabla2.yview_scroll(-1 * delta, "units")
+    tabla3.yview_scroll(-1 * delta, "units")
+    return "break"
 
 #donde escribir
 entrada_de_codigo = Entry(ventana,width=30, font=('Arial 18'))
@@ -32,14 +53,9 @@ tabla3 = Listbox(ventana,height=30,width=2 , font=('Arial 18'),bg='lightblue')
 tabla3.pack(side='left', padx=10)
 #para que entre el codigo
 
-
-
-
-
-
-
-
-
+tabla.bind("<MouseWheel>", on_mousewheel)
+tabla2.bind("<MouseWheel>", on_mousewheel)
+tabla3.bind("<MouseWheel>", on_mousewheel)
 
 def envio (event):
     #------------------------front---------------------------
@@ -51,9 +67,9 @@ def envio (event):
         codigo = codigo.replace(' ', '')
     #backend ----------------------Reconocimiento---------------------------------
     if codigo.startswith("215") and len(codigo) == 26:
+        repeticion(codigo)
         tabla.insert(END,codigo) #muestra si es un code_correo
         tabla2.insert(END,codigo[23:26])
-
 
 
     elif codigo.startswith("3600") and len(codigo) == 15:
@@ -83,6 +99,7 @@ def envio (event):
 
 
     elif codigo.count('numeroDeEnvio'):
+        repeticion(codigo[18:28])
         tabla.insert(END,codigo[18:28]) #muestra si es QR
         tabla2.insert(END,codigo[70:73])
 
@@ -105,20 +122,30 @@ def envio (event):
 entrada_de_codigo.bind('<Return>', envio)
 
 
+
 def repeticion (repeticion): #control de la tabla3
+    global contador_de_letras
     items = tabla.get(0, END)
-    for item in items:
+    for index,item in enumerate(items):
+        letra_repetida = tabla3.get(index)
         if item == repeticion:
-            tabla3.insert(END,abecedario[0])
-            contador_de_letras =+1
-            return tabla.itemconfig(END, bg='yellow')
+            if letra_repetida != '\n':
+                tabla3.insert(END, letra_repetida)
+            else:
+                tabla3.insert(END,abecedario[contador_de_letras])
+                tabla3.delete(index)
+                tabla3.insert(index,abecedario[contador_de_letras])
+                contador_de_letras += 1
+            return
+    tabla3.insert(END,'\n')
 
 
 #borrar con suprimir
 def borrar(event):
+    global contador_de_letras
     tabla.delete(0, END)
     tabla2.delete(0, END)
-    tabla3.insert(0, END)
+    tabla3.delete(0, END)
     contador_de_letras = 0
     pallet[0]= 0
 
